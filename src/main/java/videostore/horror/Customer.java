293f140a4d6fa72635bc66bfc77cc1ed520a1c53
razba <raz.bahri@nice.com>
2024-09-11 15:51:09 +0300

    const

--- src/main/java/videostore/horror/Customer.java
+++ src/main/java/videostore/horror/Customer.java
@@ -3,6 +3,9 @@ package videostore.horror;
 import java.util.*;
 
 class Customer {
+	public static final double DAY_PRICE = 1.5;
+	public static final int MIN_DAYS_FOR_REGULAR = 2;
+	public static final int MIN_DAYS_CHILDREN = 3;
 	private String name;
 	private Map<Movie, Integer> rentals = new LinkedHashMap<>(); // preserves order
 
@@ -23,34 +26,34 @@ class Customer {
 		int frequentRenterPoints = 0;
 		String result = "Rental Record for " + getName() + "\n";
 		// iterate for each rental
-		for (Movie each : rentals.keySet()) {
+		for (Movie movie : rentals.keySet()) {
 			double thisAmount = 0;
-			// determine amounts for each line
-			int dr = rentals.get(each);
-			switch (each.getPriceCode()) {
+			// determine amounts for movie line
+			int rentalDays = rentals.get(movie);
+			switch (movie.getPriceCode()) {
 				case Movie.REGULAR:
 					thisAmount += 2;
-					if (dr > 2)
-						thisAmount += (dr - 2) * 1.5;
+					if (rentalDays > MIN_DAYS_FOR_REGULAR)
+						thisAmount += (rentalDays - 2) * DAY_PRICE;
 					break;
 				case Movie.NEW_RELEASE:
-					thisAmount += dr * 3;
+					thisAmount += rentalDays * 3;
 					break;
 				case Movie.CHILDRENS:
-					thisAmount += 1.5;
-					if (dr > 3)
-						thisAmount += (dr - 3) * 1.5;
+					thisAmount += DAY_PRICE;
+					if (rentalDays > MIN_DAYS_CHILDREN)
+						thisAmount += (rentalDays - 3) * DAY_PRICE;
 					break;
 			}
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
-			if (each.getPriceCode() != null &&
-				 (each.getPriceCode() == Movie.NEW_RELEASE)
-				 && dr > 1)
+			if (movie.getPriceCode() != null &&
+				 (movie.getPriceCode() == Movie.NEW_RELEASE)
+				 && rentalDays > 1)
 				frequentRenterPoints++;
 			// show figures line for this rental
-			result += "\t" + each.getTitle() + "\t" + thisAmount + "\n";
+			result += "\t" + movie.getTitle() + "\t" + thisAmount + "\n";
 			totalAmount += thisAmount;
 		}
 		// add footer lines
